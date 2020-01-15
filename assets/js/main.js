43c518d1543eb8848751ac5e2294542ac51662f7
adarsh-av13 <dra4474@gmail.com>
2020-01-15 22:12:23 +0530

    added zenith logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 var starter = document.getElementById("starter");
 
 function loadHome() {
+    var starterText = document.getElementById("starter-text");
+    starterText.classList.add('hidden');
     var starter = document.getElementById("starter");
     starter.classList.add("starterchng");
 }
@@ -10,10 +12,15 @@ var navigatorsProperties = ['animationend', 'webkitAnimationEnd'];
 window.onload = () => {
     var starter = document.getElementById("starter");
     var nav = document.getElementById("mynav");
+    var navLeft = document.getElementById("leftn");
+    var navRight = document.getElementById("rightn");
     for (var i in navigatorsProperties) {
         starter.addEventListener(navigatorsProperties[i], function() {
             starter.classList.add('hidden');
             nav.classList.remove('hidden');
+            setTimeout(function() {
+                navRight.classList.remove('hidden');
+            }, 100);
         }, false);
     }
 };
