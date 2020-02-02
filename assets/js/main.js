a929577e5c7517b146db3ba0ddbc7572a205e74c
adarsh-av13 <dra4474@gmail.com>
2020-02-03 01:17:58 +0530

    color scheme change

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@ var starter = document.getElementById("starter");
 
 function loadHome() {
     var starter = document.getElementById("starter");
-    var navbar = document.getElementById("mynav")
-    var navRight = document.getElementById("navbarRight");
+    var navbar = document.getElementById("mynav");
     var starterTexts = document.querySelectorAll(".starter-text");
     starterTexts.forEach(starter => starter.classList.add('hidden'));
     starter.classList.add("starter-animate");
+    // starter.classList.remove("starter-flex");
+    navbar.classList.add('nav-block');
     navbar.classList.remove('hiddenav');
-    navRight.classList.remove('hiddenav');
 
 }
 
@@ -17,14 +17,11 @@ var navigatorsProperties = ['animationend', 'webkitAnimationEnd'];
 window.onload = () => {
     var starter = document.getElementById("starter");
     var hiddens = document.querySelectorAll(".hidden");
-    var navLeft = document.getElementById("navbarLeft");
 
     for (var i in navigatorsProperties) {
         starter.addEventListener(navigatorsProperties[i], function() {
-            starter.classList.add('hidden');
-            navLeft.classList.remove('hiddenav');
             hiddens.forEach((hidden) => hidden.classList.remove('hidden'));
-
+            starter.classList.add('hidden');
         }, false);
     }
 };
@@ -50,7 +47,7 @@ function checkSlide(e) {
     const f1car = document.getElementById("car");
     const start = document.getElementById("start");
     const startimg = document.getElementById("startimg");
-    if (window.scrollY > (start.offsetTop - 50)) {
+    if (window.scrollY > (start.offsetTop - 25)) {
         f1car.className = "car start";
         startimg.className = "hidden";
     } else {
@@ -63,14 +60,15 @@ function checkSlide(e) {
         const slideInAt = (window.scrollY + window.innerHeight) - 550;
         //bottom
         const imageBottom = bubblediv.offsetTop + 609;
-        const isHalfShown = slideInAt > bubblediv.offsetTop - 50;
+        const isHalfShown = slideInAt > bubblediv.offsetTop - 100;
         const isNotScrolledPast = window.scrollY < imageBottom;
 
         if (isHalfShown && isNotScrolledPast) {
             bubblediv.className = 'offset-md-1 col-md-9 col-sm-12 active bubblediv';
-        } else {
-            bubblediv.className = 'offset-md-1 col-md-9 col-sm-12 bubblediv';
-        }
+        }    
+        // } else {
+        //     bubblediv.className = 'offset-md-1 col-md-9 col-sm-12 bubblediv';
+        // }
     });
 }
 
