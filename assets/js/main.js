730533f51ee6aa4c4488f01da86dac71f713d7eb
adarsh-av13 <dra4474@gmail.com>
2020-02-04 22:28:58 +0530

    footer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,10 @@ window.onload = () => {
     }
 };
 
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  } 
 
 function debounce(func, wait = 0, immediate = true) {
     var timeout;
