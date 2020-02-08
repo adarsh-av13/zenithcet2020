fd485f602d9072e649b7949ef44866cf9155d588
adarsh-av13 <dra4474@gmail.com>
2020-02-08 14:08:42 +0530

    rest of the team + scroll back effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,11 +35,6 @@ window.onload = () => {
     }
 };
 
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 function debounce(func, wait = 0, immediate = true) {
     var timeout;
     return function () {
