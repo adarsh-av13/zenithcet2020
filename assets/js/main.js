2f9eb594921d8d70d537a3623532ce0d8506324a
adarsh-av13 <dra4474@gmail.com>
2020-04-18 23:46:27 +0530

    css grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,40 @@
-var starter = document.getElementById("starter");
+function navRes() {
+  let nav = document.getElementById("mynav");
+  if (nav.className == "mynav") nav.className += " active";
+  else nav.className = "mynav";
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+var navigatorsProperties = ["animationend", "webkitAnimationEnd"];
 
 function loadHome() {
-    var starter = document.getElementById("starter");
-    var navbar = document.getElementById("mynav");
-    var starterTexts = document.querySelectorAll(".starter-text");
-    starterTexts.forEach(starter => starter.classList.add('hidden'));
-    starter.classList.add("starter-animate");
-    navbar.classList.add('nav-block');
-    navbar.classList.remove('hiddenav');
+  let starter = document.getElementById("starter");
+  let content = document.getElementById("content");
+  starter.classList.add("starter-animate");
+  content.classList.remove("hidden");
 }
-
-var navigatorsProperties = ['animationend', 'webkitAnimationEnd'];
 
 window.onload = () => {
-    var starter = document.getElementById("starter");
-    var hiddens = document.querySelectorAll(".hidden");
-    var navbar = document.getElementById("mynav");
-    if (sessionStorage.getItem('wasVisited')) {
-        starter.classList.add('hidden');
-        navbar.classList.remove('hiddenav');
-        hiddens.forEach((hidden) => hidden.classList.remove('hidden'));
-        
-    }
-    else {
-        for (var i in navigatorsProperties) {
-            starter.addEventListener(navigatorsProperties[i], function (e) {
-                if (e.animationName == 'fade-out') {
-                    hiddens.forEach((hidden) => hidden.classList.remove('hidden'));
-                    starter.classList.add('hidden');
-                    sessionStorage.setItem('wasVisited', 1);
-                }
-            }, false);
+  let starter = document.getElementById("starter");
+  let content = document.getElementById("content");
+  if (!sessionStorage.getItem("wasVisited")) {
+    starter.classList.remove("hidden");
+    content.classList.add("hidden");
+  }
+  for (var i in navigatorsProperties) {
+    starter.addEventListener(
+      navigatorsProperties[i],
+      function (e) {
+        if (e.animationName == "fade-out") {
+          starter.classList.add("hidden");
+          sessionStorage.setItem("wasVisited", 1);
         }
-    }
+      },
+      false
+    );
+  }
 };
-
-function debounce(func, wait = 0, immediate = true) {
-    var timeout;
-    return function () {
-        var context = this,
-            args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-}
-
-function checkSlide(e) {
-    const f1car = document.getElementById("car");
-    const start = document.getElementById("start");
-    const startimg = document.getElementById("startimg");
-    const end = document.getElementById("end");
-    // if (window.scrollY > (start.offsetTop - 130)) {
-    //     f1car.className = "car start";
-    //     startimg.className = "hidden";
-    // } else {
-    //     f1car.className = "car";
-    //     startimg.className = "startpos-car";
-    // }
-    const bubbledivs = document.querySelectorAll('.bubblediv');
-    bubbledivs.forEach(bubblediv => {
-        //halfway
-        const slideInAt = (window.scrollY + window.innerHeight) - 550;
-        //bottom
-        const imageBottom = bubblediv.offsetTop + 609;
-        const isHalfShown = slideInAt > bubblediv.offsetTop - 50;
-        const isNotScrolledPast = window.scrollY < imageBottom;
-
-        if (isHalfShown && isNotScrolledPast) {
-            bubblediv.className = 'offset-md-2 col-md-8 col-sm-12 active bubblediv';
-        }
-    });
-}
-
-window.addEventListener('scroll', debounce(checkSlide));
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,14 +1,11 @@
-document.write('<a class="navbar-brand" class="navbarLeft" href="#"><img id="nav-logo" src="assets/images/nav-logo1w.png"></a>\
-<a class="navbar-brand" class="navbarLeft" href="#"><img id="nav-logo2" src="assets/images/nav-logo2w.png"></a>\
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarRight"\
-    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">\
-    <span class="navbar-toggler-icon"></span>\
-</button>\
-<div class="collapse navbar-collapse" id="navbarRight">\
-    <a class="nav-item nav-link" href="index.html">Home</a>\
-    <a class="nav-item nav-link" href="#aboutus">About</a>\
-    <a class="nav-item nav-link" href="gallery.html">Gallery</a>\
-    <a class="nav-item nav-link" href="#">Events</a>\
-    <a class="nav-item nav-link" href="team.html">The Team</a>\
-    <a class="nav-item nav-link" href="#myfooter">Contact</a>\
-</div>');
+document.write(
+  '<a href="/" id="nav-logo"><img src="../zenithcet2020/assets/images/nav-logo1w.png"></a>\
+<a href="/" id="nav-logo2"><img  src="../zenithcet2020/assets/images/nav-logo2w.png"></a>\
+    <a id="index" class="nav-link" href="index.html">Home</a>\
+    <a id="about" class="nav-link" href="index.html#aboutus">About</a>\
+    <a id="gallery" class="nav-link" href="gallery.html">Gallery</a>\
+    <a id="events" class="nav-link" href="#">Events</a>\
+    <a id="team" class="nav-link" href="team.html">The Team</a>\
+    <a id="contact" class="nav-link" href="#myfooter">Contact</a>\
+    <a href="javascript:void(0);" class="nav-icon" onclick="navRes()"><i class="fa fa-bars"></i></a>'
+);
