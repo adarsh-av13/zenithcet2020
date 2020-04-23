ace20c74e7de425f7654ad94c121f5a87c3ba7f7
adarsh-av13 <dra4474@gmail.com>
2020-04-23 13:45:42 +0530

    contact changed to contact us

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,6 +6,6 @@ document.write(
     <a id="gallery" class="nav-link" href="gallery.html">Gallery</a>\
     <a id="events" class="nav-link" href="#">Events</a>\
     <a id="team" class="nav-link" href="team.html">The Team</a>\
-    <a id="contact" class="nav-link" href="#myfooter">Contact</a>\
+    <a id="contact" class="nav-link" href="#myfooter">Contact Us</a>\
     <a href="javascript:void(0);" class="nav-icon" onclick="navRes()"><i class="fa fa-bars"></i></a>`
 );
